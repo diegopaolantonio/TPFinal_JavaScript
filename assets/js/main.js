4bae79eb9dec893fd00242dd5ed47176ca4bfb91
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2022-11-11 20:55:26 -0300

    commit 3, se mejoro el css, se agrego la toma de datos de un json local con fetch, se mejoro el ingreso de datos al cotizador

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 var costoTotal = 0;
 var precioHora = 0;
 var precioHoraHerr = 0;
+var validaHerramientas = "";
+
+var totalCotizado = 0;
+var totalCotizadoARS = 0;
+
+var tiposCambio = [];
 
 var cotizacion = {
   id: Date,
@@ -55,10 +61,10 @@ function escribirHistorico() {
   infoHoras.innerHTML = `${
     cotizacionesRealizadas[cotizacionesRealizadas.length - 1].horas
   }`;
-  infoCostoTotal.innerHTML = `${new Intl.NumberFormat('es-PE', {
-    style: 'decimal',
+  infoCostoTotal.innerHTML = `${new Intl.NumberFormat("es-PE", {
+    style: "decimal",
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2
+    maximumFractionDigits: 2,
   }).format(
     cotizacionesRealizadas[cotizacionesRealizadas.length - 1].costoTotal
   )}`;
@@ -67,35 +73,48 @@ function escribirHistorico() {
   mensajeD.style.display = "block";
 }
 
-function costoHora(val) {
-  let precio = 0;
-  if (val == "programador" || val == "Programador" || val == "PROGRAMADOR") {
-    console.log(1);
-    precio = 20;
-  } else if (
-    val == "electricista" ||
-    val == "Electricista" ||
-    val == "ELECTRICISTA"
-  ) {
-    console.log(2);
-    precio = 15;
-  } else if (
-    val == "asistencia tecnica" ||
-    val == "Asistencia Tecnica" ||
-    val == "ASISTENCIA TECNICA"
-  ) {
-    console.log(3);
-    precio = 14;
+function costoHoraFunc() {
+  if (precioHora == 20) {
+    trabajoProgId.innerHTML = `<input class="btn btn-secondary" type="button" value="Programador" id="trabajoProg">`;
+  } else {
+    trabajoProgId.innerHTML = `<input class="btn btn-primary" type="button" value="Programador" id="trabajoProg">`;
   }
 
-  return precio;
+  if (precioHora == 15) {
+    trabajoElectId.innerHTML = `<input class="btn btn-secondary" type="button" value="Electricista" id="trabajoElect">`;
+  } else {
+    trabajoElectId.innerHTML = `<input class="btn btn-primary" type="button" value="Electricista" id="trabajoElect">`;
+  }
+
+  if (precioHora == 14) {
+    trabajoAsistId.innerHTML = `<input class="btn btn-secondary" type="button" value="Asistencia Tecnica" id="trabajoAsist">`;
+  } else {
+    trabajoAsistId.innerHTML = `<input class="btn btn-primary" type="button" value="Asistencia Tecnica" id="trabajoAsist">`;
+  }
+
+  console.log(precioHora);
+}
+
+function herramientasSiNo() {
+  if (validaHerramientas == "Si") {
+    herramientasSiId.innerHTML = `<input class="btn btn-secondary" type="button" value="Si" id="herramientasSi">`;
+  } else {
+    herramientasSiId.innerHTML = `<input class="btn btn-primary" type="button" value="Si" id="herramientasSi">`;
+  }
+
+  if (validaHerramientas == "No") {
+    herramientasNoId.innerHTML = `<input class="btn btn-secondary" type="button" value="No" id="herramientasNo">`;
+  } else {
+    herramientasNoId.innerHTML = `<input class="btn btn-primary" type="button" value="No" id="herramientasNo">`;
+  }
+
+  console.log(validaHerramientas);
 }
 
 function sincronizarConLocalStorage() {
   localStorage.setItem("cotizacion", JSON.stringify(cotizacionesRealizadas));
 }
 
-//if (typeof window === "object") {
 window.addEventListener("DOMContentLoaded", function () {
   // NUEVO: Contenido cargado
   console.log("EL DOM SE CARGO");
@@ -106,81 +125,102 @@ window.addEventListener("DOMContentLoaded", function () {
   if (cotizacionesRealizadas.length > 0) {
     escribirHistorico();
   }
+});
 
-  const formatearSiNo = (val) => {
-    if (val == "si" || val == "SI" || val == "sI" || val == "Si") {
-      val = "Si";
-      console.log("Si");
-    } else if (val == "no" || val == "No" || val == "NO" || val == "nO") {
-      val = "No";
-      console.log("No");
-    }
-    return val;
-  };
+const checkNumero = (val) => {
+  let number = parseInt(val);
 
-  const checkString = (val) => {
-    if (/[a-zA-z]+$/.test(val) && val.length >= 1) {
-      return true;
-    }
+  if (/[^0-9]/g.test(number) && typeof number === "number") {
     return false;
-  };
+  }
+  return true;
+};
 
-  const checkNumero = (val) => {
-    let number = parseInt(val);
+const cargarTipoCambio = async () => {
+  const datosFetch = await fetch("/assets/db/cambio.json")
+    .then((resp) => resp.json())
+    .then((data) => data);
 
-    if (/[^0-9]/g.test(number) && typeof number === "number") {
-      return false;
-    }
-    return true;
-  };
+  tiposCambio = datosFetch;
+
+  console.log(tiposCambio);
+
+  tiposCambio.forEach((element, index) => {
+    // tiposCambioUSD.innerHTML += `<p>${element.tipo}</p>
+    // ${element.categoria1}  ${element.categoria2}</p>
+    // ${element.valor1}  ${element.valor2}</p>`;
+
+    tiposCambioUSD.innerHTML += `<hr>
+    <p>${element.tipo}</p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th class="text-center" scope="col">${element.categoria1}</th>
+          <th class="text-center" scope="col">${element.categoria2}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="text-center" scope="col">${element.valor1}</td>
+          <td class="text-center" scope="col">${element.valor2}</td>
+        </tr>
+      </tbody>
+    </table>`;
+  });
+};
+
+function cotizador() {
+  if (cotizacionesRealizadas.length > 0) {
+    escribirHistorico();
+  }
 
   // Validamos el campo referencia
-  referencia.addEventListener("input", (e) => {
-    /*    let validaReferencia = checkString(e.target.value);
-    if (validaReferencia) {
-      error_referencia.style.display = "none";
-      error_referencia.innerHTML = ``;
-    } else {
-      error_referencia.style.display = "block";
-      error_referencia.innerHTML = `Debe ingresar numeros o letras`;
-    }
-  */
-  });
+  referencia.addEventListener("input", (e) => {});
+
+  costoHoraFunc();
+  herramientasSiNo();
 
   // Validamos el campo trabajo
-  trabajo.addEventListener("input", (e) => {
-    let validaTrabajo = checkString(e.target.value);
-    precioHora = costoHora(e.target.value);
-    if (
-      validaTrabajo &&
-      (precioHora == 20 || precioHora == 15 || precioHora == 14)
-    ) {
-      error_trabajo.style.display = "none";
-      error_trabajo.innerHTML = ``;
-    } else {
-      error_trabajo.style.display = "block";
-      error_trabajo.innerHTML = `Debe ingrese opcion correcta`;
-    }
+  trabajoProg.addEventListener("click", (e) => {
+    precioHora = 20;
+    console.log(precioHora);
+    trabajo = "Programador";
+
+    costoHoraFunc();
+
+    console.log("Programador");
+  });
+
+  trabajoElect.addEventListener("click", (e) => {
+    precioHora = 15;
+    trabajo = "Electricista";
+
+    costoHoraFunc();
+
+    console.log("Electricista");
+  });
+
+  trabajoAsist.addEventListener("click", (e) => {
+    precioHora = 14;
+    trabajo = "Asistencia Tecnica";
+
+    costoHoraFunc();
+
+    console.log("Asistencia Tecnica");
   });
 
   // Validamos el campo herramientas
-  herramientas.addEventListener("input", (e) => {
-    let validaHerramientas = checkString(e.target.value);
-    if (validaHerramientas) {
-      validaHerramientas = formatearSiNo(e.target.value);
-      if (validaHerramientas == "Si") {
-        precioHoraHerr = 1.25;
-        error_herramientas.style.display = "none";
-        error_herramientas.innerHTML = ``;
-      } else if (validaHerramientas == "No") {
-        precioHoraHerr = 1;
-        error_herramientas.style.display = "none";
-        error_herramientas.innerHTML = ``;
-      }
-    } else {
-      error_herramientas.style.display = "block";
-      error_herramientas.innerHTML = `Debe ingresar alguna opcion`;
-    }
+
+  herramientasSi.addEventListener("click", (e) => {
+    validaHerramientas = "Si";
+    precioHoraHerr = 1.25;
+    herramientasSiNo();
+  });
+
+  herramientasNo.addEventListener("click", (e) => {
+    validaHerramientas = "No";
+    precioHoraHerr = 1;
+    herramientasSiNo();
   });
 
   // Validamos el campo personas
@@ -215,8 +255,12 @@ window.addEventListener("DOMContentLoaded", function () {
       error_horas.innerHTML = ``;
     } else {
       error_horas.style.display = "block";
-      error_horas.innerHTML = `Debe ingresar solo numeros`;
+      error_horas .innerHTML = `Debe ingresar solo numeros`;
     }
+  });
+
+  cambioAUsar.addEventListener("input", (e) => {
+    let validaCambio = checkNumero(e.target.value);
   });
 
   // Seleccionamos el formulario
@@ -241,8 +285,7 @@ window.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
     // guardamos datos que ingresamos en los input
     let referencia = document.querySelector("#referencia").value;
-    let trabajo = document.querySelector("#trabajo").value;
-    let herramientas = document.querySelector("#herramientas").value;
+    let herramientas = validaHerramientas;
     let personas = document.querySelector("#personas").value;
     let dias = document.querySelector("#dias").value;
     let horas = document.querySelector("#horas").value;
@@ -271,46 +314,6 @@ window.addEventListener("DOMContentLoaded", function () {
 
       agregarCotizacion();
 
-      console.log(cotizacion);
-      console.log(cotizaciones);
-      console.log(cotizacionesRealizadas);
-    } else {
-      alert("Debe llenar los campos");
-    }
-  });
-
-  formulario.addEventListener("submit", function (e) {
-    e.preventDefault();
-    // guardamos datos que ingresamos en los input
-    let referencia = document.querySelector("#referencia").value;
-    let trabajo = document.querySelector("#trabajo").value;
-    let herramientas = document.querySelector("#herramientas").value;
-    let personas = document.querySelector("#personas").value;
-    let dias = document.querySelector("#dias").value;
-    let horas = document.querySelector("#horas").value;
-
-    if (referencia && precioHora && herramientas && personas && dias && horas) {
-      costoTotal = dias * precioHora * precioHoraHerr * personas * horas;
-      console.log(costoTotal);
-      console.log(precioHoraHerr);
-
-      const generarId = () => Math.random().toString(36).substring(2, 18);
-
-      cotizacion.id = generarId();
-      cotizacion.referencia = referencia;
-      cotizacion.trabajo = trabajo;
-      cotizacion.herramientas = herramientas;
-      cotizacion.personas = personas;
-      cotizacion.dias = dias;
-      cotizacion.horas = horas;
-      cotizacion.costoTotal = costoTotal;
-
-      let cotizaciones = [];
-
-      cotizaciones.push(cotizacion);
-
-      cotizacionesRealizadas = [...cotizacionesRealizadas, ...cotizaciones];
-
       document.location.reload();
 
       console.log(cotizacion);
@@ -320,37 +323,75 @@ window.addEventListener("DOMContentLoaded", function () {
       alert("Debe llenar los campos");
     }
   });
-});
+
+  formulario.addEventListener("reset", function (e) {
+    e.preventDefault();
+
+    document.location.reload();
+  });
+
+  formularioCambio.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    console.log("Diego Ingresado");
+
+    // guardamos datos que ingresamos en los input
+    let cambioAUsar = document.querySelector("#cambioAUsar").value;
+  
+    totalCotizadoARS = totalCotizado*cambioAUsar;
+
+    console.log(totalCotizado);
+    console.log(cambioAUsar);
+    console.log(totalCotizadoARS);
+    
+    const totalCotizadoARSFormateado = new Intl.NumberFormat("es-PE", {
+      style: "decimal",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(totalCotizadoARS);
+    
+    console.log(totalCotizadoARSFormateado);
+    
+    infoTotalCotizadoARS.innerHTML = `${totalCotizadoARSFormateado}`;
+    
+  });
+}
+
+cotizador();
 
 function agregarCotizacion() {
-  let totalCotizado = 0;
-
   tabla.innerHTML = ``;
-  cotizacionesRealizadas.forEach((element, index) => {
+  cotizacionesRealizadas.forEach((element) => {
     totalCotizado += element.costoTotal;
 
     tabla.innerHTML += `<tr>
              <td class="text-center">${element.referencia}</td>
              <td class="text-center">${element.trabajo}</td>
              <td class="text-center">${element.herramientas}</td>
-             <td class="text-center">USD ${new Intl.NumberFormat('es-PE', {
-              style: 'decimal',
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
-            }).format(element.costoTotal)}</td>
-             <td class="text-center"><a href="#" id="${element.id}" class="borrarCotizacion" data-id="1">X</a></td>
+             <td class="text-center">USD ${new Intl.NumberFormat("es-PE", {
+               style: "decimal",
+               minimumFractionDigits: 2,
+               maximumFractionDigits: 2,
+             }).format(element.costoTotal)}</td>
+             <td class="text-center"><a href="#" id="${
+               element.id
+             }" class="borrarCotizacion" data-id="1">X</a></td>
            </tr>`;
   });
 
-  const totalCotizadoFormateado = new Intl.NumberFormat('es-PE', {
-    style: 'decimal',
+  cargarTipoCambio();
+
+  console.log(tiposCambio.valor1);
+
+  const totalCotizadoFormateado = new Intl.NumberFormat("es-PE", {
+    style: "decimal",
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2
+    maximumFractionDigits: 2,
   }).format(totalCotizado);
-  
+
   console.log(totalCotizadoFormateado);
 
-  infoTotalCotizado.innerHTML = `${totalCotizadoFormateado}`;
+  infoTotalCotizado.innerHTML += `${totalCotizadoFormateado}`;
 
   // Seleccionamos el boton eliminar
   let buttonDelete = document.querySelectorAll(".borrarCotizacion");
@@ -373,3 +414,8 @@ function agregarCotizacion() {
 
   sincronizarConLocalStorage();
 }
+
+
+
+
+
